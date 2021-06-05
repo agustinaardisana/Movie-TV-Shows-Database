da690caf239d12db627cb4dec8fbdc27a9bb1899
agustinaardisana <agustinaardisana@gmail.com>
2021-06-05 11:57:29 -0500

    Footer uodated, socila links added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 //
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+//
 import { FlexCenter } from "./Commons";
 
 const FooterContainer = styled(FlexCenter)`
@@ -10,10 +13,38 @@ const FooterContainer = styled(FlexCenter)`
   color: ${(props) => props.theme.colors.text};
 `;
 
+const StyledLink = styled.a`
+  color: ${(props) => props.theme.colors.link};
+  margin: ${(props) => props.theme.spacing.sm};
+
+  &:hover {
+    color: ${(props) => props.theme.colors.text};
+  }
+`;
+
 const Footer = () => {
   return (
     <FooterContainer as="footer">
-      Hecho con ☕ y 🎶 en Ada ITW por Agus Ardisana
+      Hecho con ☕ y 🎶 en
+      <StyledLink href="https://adaitw.org/" target="_blank">
+        Ada ITW
+      </StyledLink>
+      por
+      <StyledLink
+        href="https://www.linkedin.com/in/agustinaardisana/"
+        target="_blank"
+      >
+        Agus Ardisana
+      </StyledLink>
+      <StyledLink href="https://github.com/agustinaardisana" target="_blank">
+        <GitHubIcon />
+      </StyledLink>
+      <StyledLink
+        href="https://www.linkedin.com/in/agustinaardisana/"
+        target="_blank"
+      >
+        <LinkedInIcon />
+      </StyledLink>
     </FooterContainer>
   );
 };
