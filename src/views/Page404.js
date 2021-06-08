1b28b27d5a5adf6f6b1287cb2e2f2d9c4d64728f
agustinaardisana <agustinaardisana@gmail.com>
2021-06-07 19:58:31 -0500

    responsive working for search and 404 components

--- src/views/Page404.js
+++ src/views/Page404.js
@@ -6,15 +6,26 @@ import error404 from "../assets/404.svg";
 
 const Container = styled(FlexCenter)`
   padding: ${(props) => props.theme.spacing.lg};
+  flex-direction: column;
 `;
 
 const Img = styled(StyledImg)`
   /* css */
 `;
 
+const StyledTitle = styled.h2`
+  color: ${(props) => props.theme.colors.primary};
+  font-family: ${(props) => props.theme.fonts.families.title};
+  margin-bottom: ${(props) => props.theme.spacing.md};
+  font-size: ${(props) => props.theme.fonts.sizes.h2};
+`;
+
 const Page404 = () => {
   return (
     <Container>
+      <StyledTitle>
+        Lo sentimos, no podemos encontrar la página a la que intentas ir.
+      </StyledTitle>
       <Img src={error404} alt="Error 404, página no encontrada" />
     </Container>
   );
